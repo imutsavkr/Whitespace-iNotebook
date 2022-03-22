74ba676e17679bf1c0e6aa1b6cc8b887050ba67e
Utsav Kumar <45901934+Utsav418@users.noreply.github.com>
2022-03-22 16:53:06 +0530

    Fetched Notes Item

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,7 @@
-import React, { useContext } from 'react'
-import noteContext from "../context/notes/NoteContext"
-
-
-
+import React from 'react'
+import Notes from "./Notes"
 export const Home = () => {
-    const context = useContext(noteContext);
-const { notes, serNotes } = context
+ 
     return (
         <div>
             <div className="container my-3">
@@ -27,12 +23,7 @@ const { notes, serNotes } = context
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
-            <div className="container my-33">
-                <h1>Your Notes</h1>
-                {notes.map((note) => {
-                    return note.title;
-                })}
-            </div>
+            <Notes/>
         </div>
     )
 }
