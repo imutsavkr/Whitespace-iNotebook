19a989d49023b14558baf36197acbe9c6f8e34fa
Utsav Kumar <45901934+Utsav418@users.noreply.github.com>
2022-03-23 13:35:18 +0530

    Login and Signup Componenets dded

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -16,8 +16,7 @@ const NoteState = (props) => {
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjIzNDRlNzhiY2ZjNGE1YzA5YTM1MjkwIn0sImlhdCI6MTY0NzkyNjk5N30.2odLK_4DjNf61eOdEAAaoVCmDowo7BZEQUJEWQh_DYI"
       }
     });
-    const json = await response.json()
-    console.log(json)
+    const json = await response.json() 
     setNotes(json)
   }
 
@@ -31,22 +30,10 @@ const NoteState = (props) => {
         'Content-Type': 'application/json',
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjIzNDRlNzhiY2ZjNGE1YzA5YTM1MjkwIn0sImlhdCI6MTY0NzkyNjk5N30.2odLK_4DjNf61eOdEAAaoVCmDowo7BZEQUJEWQh_DYI"
       },
-      body: JSON.stringify({ title, description, tag })
+      body: JSON.stringify({title, description, tag})
     });
-    const json = await response.json();
-    console.log(json);
 
-
-    console.log("Adding a new note")
-    const note = {
-      "_id": "61322f119553781a8ca8d0e08",
-      "user": "6131dc5e3e4037cd4734a0664",
-      "title": title,
-      "description": description,
-      "tag": tag,
-      "date": "2021-09-03T14:20:09.668Z",
-      "__v": 0
-    };
+    const note = await response.json();
     setNotes(notes.concat(note))
   }
 
@@ -60,13 +47,11 @@ const NoteState = (props) => {
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjIzNDRlNzhiY2ZjNGE1YzA5YTM1MjkwIn0sImlhdCI6MTY0NzkyNjk5N30.2odLK_4DjNf61eOdEAAaoVCmDowo7BZEQUJEWQh_DYI"
       }
     });
-    const json = response.json();
-    console.log(json)
-
-    console.log("Deleting the note with id" + id);
+    const json = response.json(); 
     const newNotes = notes.filter((note) => { return note._id !== id })
     setNotes(newNotes)
   }
+
   // Edit a Note
   const editNote = async (id, title, description, tag) => {
     // API Call 
@@ -74,12 +59,11 @@ const NoteState = (props) => {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
-        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjIzNDRlNzhiY2ZjNGE1YzA5YTM1MjkwIn0sImlhdCI6MTY0NzkyNjk5N30.2odLK_4DjNf61eOdEAAaoVCmDowo7BZEQUJEWQh_DYI"
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEzMWRjNWUzZTQwMzdjZDQ3MzRhMDY2In0sImlhdCI6MTYzMDY2OTU5Nn0.hJS0hx6I7ROugkqjL2CjrJuefA3pJi-IU5yGUbRHI4Q"
       },
       body: JSON.stringify({title, description, tag})
     });
-    const json = await response.json();
-    console.log(json)
+    const json = await response.json(); 
 
      let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
